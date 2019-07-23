7685a65480191af3265a4fc3b1b7505c7ff5d2df
Anton Bugaev <32480392+aabugaev@users.noreply.github.com>
2019-07-24 00:11:43 +0300

    Add files via upload

--- Appender_dateparse.py
+++ Appender_dateparse.py
@@ -2,7 +2,21 @@
 # coding: utf-8
 
 # In[11]:
+import subprocess
+import sys
 
+def install(package):
+    subprocess.call([sys.executable, "-m", "pip", "install", package])
+
+try:
+    import pandas as pd
+    import numpy as np
+    import xlrd
+
+except:
+    install('pandas')
+    install('numpy')
+    install('xlrd')
 
 import pandas as pd
 import numpy as np
